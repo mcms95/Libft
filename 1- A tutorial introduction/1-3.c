819d23c3509727aeb6327454efd5161cb0a4d55f
mcms95 <nuno.mcms21@gmail.com>
2023-06-15 16:18:56 +0100

    1

--- 1- A tutorial introduction/1-3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() /* Fahrenheit-Celsius table */
-{  
-    int fahr;
-    char header[]= "FAHR  CELCIUS\n------------";
-    printf("%s\n", header);
-    for (fahr = 0; fahr <= 300; fahr = fahr + 40)
-        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-}
--- 1- A tutorial introduction/1-5.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-main() /* Fahrenheit-Celsius table */
-{
-    int fahr;
-    for (fahr = 300; fahr >= 0; fahr -= 20)
-        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-}
