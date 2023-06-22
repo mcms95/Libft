a6bf12c4d2bdca313f014eebed693a062e605bd8
mcms95 <nuno.mcms21@gmail.com>
2023-06-22 09:25:38 +0100

    "Formatted & Makefile"

--- libft.h
+++ libft.h
@@ -24,7 +24,6 @@ char *ft_strdup(const char *s);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
-char **ft_split(char const *s, char c);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char*));
 size_t strlcat(char *dest, const char *src, size_t size);
